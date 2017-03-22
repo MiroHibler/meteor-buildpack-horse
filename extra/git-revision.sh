47cfde5240d27ce5a44da8917159553f471a7ac8
Miroslav Hibler <mhibler@gmail.com>
2017-03-22 10:31:48 +0100

    Showing

--- extra/git-revision.sh
+++ extra/git-revision.sh
@@ -3,10 +3,13 @@
 #
 #echo "-----> Adding git revision number to environmnet variable"
 echo "-----> Showing environmnet variables"
-#cat > "$APP_CHECKOUT_DIR"/.profile.d/git_revision.sh <<EOF
+echo "-----> 1.1 APP_CHECKOUT_DIR=$APP_CHECKOUT_DIR"
+cat > "$APP_CHECKOUT_DIR"/.profile.d/git_revision.sh <<EOF
   #!/bin/bash
   #git_revision=`git rev-parse HEAD`
   #export GIT_REVISION="$git_revision"
   #echo "$git_revision"
-  env
-#EOF
+  echo "-----> 2. APP_CHECKOUT_DIR=$APP_CHECKOUT_DIR"
+  cd "$APP_CHECKOUT_DIR"
+  ls -al
+EOF
