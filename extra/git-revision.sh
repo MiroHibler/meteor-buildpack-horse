81a4a8d1b47d314ec441e6d7a5da4a52b05867c6
Miroslav Hibler <mhibler@gmail.com>
2017-03-22 10:47:51 +0100

    Showing all build-time env vars

--- extra/git-revision.sh
+++ extra/git-revision.sh
@@ -3,10 +3,10 @@
 #
 #echo "-----> Adding git revision number to environmnet variable"
 echo "-----> Showing environmnet variables"
-echo "-----> 1. APP_CHECKOUT_DIR=$APP_CHECKOUT_DIR"
-cat > "$APP_CHECKOUT_DIR"/.profile.d/git_revision.sh <<EOF
-  #!/bin/bash
-  echo "-----> 2. APP_CHECKOUT_DIR=$APP_CHECKOUT_DIR"
-  cd "$APP_CHECKOUT_DIR"
-  ls -al
-EOF
+env
+# cat > "$APP_CHECKOUT_DIR"/.profile.d/git_revision.sh <<EOF
+#   #!/bin/bash
+#   echo "-----> 2. APP_CHECKOUT_DIR=$APP_CHECKOUT_DIR"
+#   cd "$APP_CHECKOUT_DIR"
+#   ls -al
+# EOF
