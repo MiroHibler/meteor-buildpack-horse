19e45a09f4e8b25bccf7b1b1538a00b153269038
Miroslav Hibler <mhibler@gmail.com>
2017-03-22 09:51:13 +0100

    Showing all environment variables

--- extra/git-revision.sh
+++ extra/git-revision.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 #
 #
-echo "-----> Adding git revision number to environmnet variable"
+#echo "-----> Adding git revision number to environmnet variable"
+echo "-----> Showing environmnet variables"
 #cat > "$APP_CHECKOUT_DIR"/.profile.d/git_revision.sh <<EOF
   #!/bin/bash
-  git_revision=`git rev-parse HEAD`
+  #git_revision=`git rev-parse HEAD`
   #export GIT_REVISION="$git_revision"
-  echo "$git_revision"
+  #echo "$git_revision"
+  env
 #EOF
