cd3bd623ad55e67ed9189a0e4bed67de6b55682c
Miroslav Hibler <mhibler@gmail.com>
2017-03-22 17:30:14 +0100

    Reintroducing Heroku environment variables

--- extra/git-revision.sh
+++ extra/git-revision.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 #
 #
-#echo "-----> Adding git revision number to environmnet variable"
-echo "-----> Showing $APP_CHECKOUT_DIR contents"
-ls -al
-# cat > "$APP_CHECKOUT_DIR"/.profile.d/git_revision.sh <<EOF
-#   #!/bin/bash
-#   echo "-----> 2. APP_CHECKOUT_DIR=$APP_CHECKOUT_DIR"
-#   cd "$APP_CHECKOUT_DIR"
-#   ls -al
-# EOF
+echo "-----> Adding profile script to show heroku environment variables"
+cat > "$APP_CHECKOUT_DIR"/.profile.d/git_revision.sh <<EOF
+  #!/bin/bash
+  echo "-----> Showing heroku environment variables"
+  env
+EOF
